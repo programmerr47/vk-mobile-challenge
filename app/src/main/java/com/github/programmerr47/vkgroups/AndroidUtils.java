240262d018160d4f4fb451a185e9d48396fd25bd
ms <mikhail.spitsin@sidenis.com>
2016-01-15 10:37:12 +0300

    Added new Behavior to FAB (slide down, slide up on scroll)

--- app/src/main/java/com/github/programmerr47/vkgroups/AndroidUtils.java
+++ app/src/main/java/com/github/programmerr47/vkgroups/AndroidUtils.java
@@ -1,6 +1,7 @@
 package com.github.programmerr47.vkgroups;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Typeface;
 
 /**
@@ -11,5 +12,14 @@ public class AndroidUtils {
 
     public static Typeface getAssetsTypeface(Context context, Constants.Font font) {
         return Typeface.createFromAsset(context.getAssets(), Constants.ASSETS_FONTS_DIR + font.getFontName());
+    }
+
+    public static int toolbarDefaultHeight(Context context) {
+        final TypedArray styledAttributes = context.getTheme().obtainStyledAttributes(
+                new int[]{R.attr.actionBarSize});
+        int toolbarHeight = (int) styledAttributes.getDimension(0, 0);
+        styledAttributes.recycle();
+
+        return toolbarHeight;
     }
 }
