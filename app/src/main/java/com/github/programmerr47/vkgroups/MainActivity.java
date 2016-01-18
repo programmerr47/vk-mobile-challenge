0a12fc039dd527ef0efef36701e03d2ee9284d42
Michael <spitsinmichael@gmail.com>
2016-01-19 01:09:18 +0300

    - removed vector useless icons - changed primary and accent colors - change Fab behavior to support snackbar - added plus icon white for fab in groups - select "groups" on start in drawer - added ripple effect to list view items - changed gradient for drawer header

--- app/src/main/java/com/github/programmerr47/vkgroups/MainActivity.java
+++ app/src/main/java/com/github/programmerr47/vkgroups/MainActivity.java
@@ -158,6 +158,7 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_communities);
 
         FragmentManager fm = getSupportFragmentManager();
         groupsFragment = GroupsFragment.createInstance();
--- app/src/main/java/com/github/programmerr47/vkgroups/view/behavior/FABDependToolbarScrollBehavior.java
+++ app/src/main/java/com/github/programmerr47/vkgroups/view/behavior/FABDependToolbarScrollBehavior.java
@@ -13,23 +13,25 @@ import static com.github.programmerr47.vkgroups.AndroidUtils.toolbarDefaultHeigh
  * @author Michael Spitsin
  * @since 2016-01-15
  */
-public class FABDependToolbarScrollBehavior extends CoordinatorLayout.Behavior<FloatingActionButton> {
+public class FABDependToolbarScrollBehavior extends FloatingActionButton.Behavior {
 
     private int toolbarHeight;
 
     public FABDependToolbarScrollBehavior(Context context, AttributeSet attrs) {
-        super(context, attrs);
+        super();
         toolbarHeight = toolbarDefaultHeight(context);
     }
 
     @Override
     public boolean layoutDependsOn(CoordinatorLayout parent, FloatingActionButton fab, View dependency) {
-        return super.layoutDependsOn(parent, fab, dependency) || (dependency instanceof AppBarLayout);
+        return super.layoutDependsOn(parent, fab, dependency) ||
+                (dependency instanceof AppBarLayout);
     }
 
     @Override
     public boolean onDependentViewChanged(CoordinatorLayout parent, FloatingActionButton fab, View dependency) {
         boolean returnValue = super.onDependentViewChanged(parent, fab, dependency);
+
         if (dependency instanceof AppBarLayout) {
             CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams) fab.getLayoutParams();
             int fabBottomMargin = lp.bottomMargin;
@@ -37,6 +39,7 @@ public class FABDependToolbarScrollBehavior extends CoordinatorLayout.Behavior<F
             float ratio = dependency.getY() / toolbarHeight;
             fab.setTranslationY(-distanceToScroll * ratio);
         }
+
         return returnValue;
     }
 }
